2c2670e3d36eccaf4b4f34c1f10971ed07fe8969
rav08k <88479305+rav08k@users.noreply.github.com>
2021-08-05 14:18:34 +0530

    Update Q10.js

--- Q10.js
+++ Q10.js
@@ -6,8 +6,6 @@ level = level;
 }
 // test code:
 var hero2 = new Hero;
-var hero2 = new Hero;
-var hero2 = new Hero;
 var hero1 = new Hero;
 
 hero1.name = 'ravi';
@@ -18,4 +16,4 @@ hero2.level = 24;
 console.log(hero1); // output : name = ravi , level = 4
 console.log(hero2); // output : name = avi , level = 24
 
-//yes this code is correct as here name and level is defined in the Hero function and if we define a new function a new function with the  help of the  constructor function each new function gets their own copy of name and level variables.
+//yes this code is correct as here name and level is defined in the Hero function and if we define a new function with the help of the constructor function each new function gets their own copy of name and level variables.
